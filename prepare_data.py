2f8f48fb8aa15bc19878e3bd65b6504e963e268d
Sampo Pyysalo <sampo.pyysalo@utu.fi>
2021-08-25 19:04:06 +0300

    Use Dataset.from_dict instead of load_dataset, add caching options

--- prepare_data.py
+++ prepare_data.py
@@ -1,12 +1,16 @@
 import sys
 
+import datasets
+
 from pathlib import Path
 from functools import wraps
 from time import time
 from argparse import ArgumentParser
 
-from datasets import load_dataset
+from datasets import Dataset, DatasetDict
+from datasets import load_dataset, set_caching_enabled
 from transformers import AutoTokenizer
+from tqdm import tqdm
 
 
 def argparser():
@@ -25,6 +29,18 @@ def argparser():
         '--tokenizer',
         required=True,
         help='tokenizer name or path'
+    )
+    ap.add_argument(
+        '--in_memory_max_size',
+        type=int,
+        default=None,
+        help='max size of dataset to keep in memory (in GB)'
+    )
+    ap.add_argument(
+        '--disable_cache',
+        default=False,
+        action='store_true',
+        help='perform processing in memory (no disk cache)'
     )
     ap.add_argument(
         '--num_workers',
@@ -46,19 +62,25 @@ def timed(f, out=sys.stderr):
     def wrapper(*args, **kwargs):
         start = time()
         result = f(*args, **kwargs)
-        print('{} completed in {:.1f} sec'.format(f.__name__, time()-start),
-              file=out)
+        print(f'{f.__name__} completed in {time()-start:.1f} sec',
+              file=out, flush=True)
         return result
     return wrapper
 
 
 @timed
-def load_text(paths):
-    dataset = load_dataset(
-        'text',
-        data_files=paths
-    )
-    return dataset
+def load_text(paths, args):
+    texts = []
+    for p in tqdm(paths):
+        with open(p) as f:
+            texts.extend([s for s in f.read().splitlines() if s])
+    print(f'read {len(texts)} lines from {len(paths)} files.')
+    dataset = Dataset.from_dict({
+        'text': texts
+    })
+    datasets = DatasetDict()
+    datasets['train'] = dataset
+    return datasets
 
 
 @timed
@@ -98,9 +120,16 @@ def prepare_examples(data, args):
     
 def main(argv):
     args = argparser().parse_args(argv[1:])
-    
+
+    if args.in_memory_max_size:
+        max_bytes = 1024**3 * args.in_memory_max_size
+        datasets.config.IN_MEMORY_MAX_SIZE = max_bytes
+
+    if args.disable_cache:
+        set_caching_enabled(False)
+
     paths = [str(x) for x in Path(args.data).glob("**/*.txt")]
-    data = load_text(paths)
+    data = load_text(paths, args)
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
     data = tokenize_text(data, tokenizer, args)
