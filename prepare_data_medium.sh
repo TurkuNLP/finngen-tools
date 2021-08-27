e96ebca5032ab4f01f1dc1ca9f702607b9bc8463
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2021-08-27 08:40:25 +0300

    Initial import

---
+++ prepare_data_medium.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#SBATCH --partition=medium
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=128
+#SBATCH --account=project_2004600
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+
+DATADIR="texts"
+OUTDIR="processed"
+TOKENIZER="tokenizer"
+
+rm -f logs/latest.out logs/latest.err
+ln -s $SLURM_JOBID.out logs/latest.out
+ln -s $SLURM_JOBID.err logs/latest.err
+
+module purge
+module load pytorch
+
+# mahti nvme is only available on GPU nodes, so use scratch and wipe
+# on exit
+CACHEDIR="/scratch/$SLURM_JOB_ACCOUNT/datasets_cache_$SLURM_JOBID"
+
+export HF_DATASETS_CACHE="$CACHEDIR"
+echo "CACHE: $HF_DATASETS_CACHE"
+
+############################## TODO UNCOMMENT ##############################
+
+# function on_exit {
+#     rm -f "$CACHEDIR"
+# }
+# trap on_exit EXIT
+
+NUM_WORKERS=$SLURM_CPUS_PER_TASK
+
+echo "START $SLURM_JOBID: $(date)"
+
+python prepare_data.py \
+       --data "$DATADIR" \
+       --tokenizer "$TOKENIZER" \
+       --output_dir "$OUTDIR" \
+       --num_workers $NUM_WORKERS
+
+seff $SLURM_JOBID
+
+echo "END $SLURM_JOBID: $(date)"
