fd0424f635ccf4bdfb973e9a90fd2ce7f8b4aa4a
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2021-09-13 11:46:22 +0300

    Update iterablebinarydataset.py

--- iterablebinarydataset.py
+++ iterablebinarydataset.py
@@ -23,33 +23,43 @@ class IterableBinaryDataset(IterableDataset):
         self.vecf = open(vecfn, 'rb')
         self.index = 0
 
+    def __len__(self):
+        return self.num_rows
+
     def __iter__(self):
         return self
 
     def __next__(self):
-        example = {}
+        if self.index == self.num_rows:
+            # completed a pass, confirm and cycle to beginning
+            if len(self.vecf.read(1)) != 0:
+                raise ValueError(f'extra data in {self.vecf.name}')
+            self.vecf.seek(0)
+            self.index = 0
         fmt = f'<{self.dim}H'
         size = struct.calcsize(fmt)
+        example = {}
         for n in self.column_names:
             data = self.vecf.read(size)
             if len(data) != size:
-                if len(data) == 0 and self.index == self.num_rows:
-                    raise StopIteration    # expected EOF
-                else:
-                    raise EOFError    # unexpected EOF
+                raise EOFError    # unexpected EOF
             vec = struct.unpack(fmt, data)
             example[n] = vec
         self.index += 1
         return example
 
 
-def main(argv):
-    directory = argv[1]
+def load_binary_dataset(directory):
     paths = [str(p) for p in Path(directory).glob("*.vec")]
     datasets = {}
     for p in paths:
         name = os.path.splitext(os.path.basename(p))[0]
         datasets[name] = IterableBinaryDataset(p)
+    return datasets
+
+
+def main(argv):
+    datasets = load_binary_dataset(argv[1])
     for n, d in datasets.items():
         for e in d:
             print(e)
