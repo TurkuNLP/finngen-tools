450553887cd40b446ef7304e67408d57811b99f5
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2022-05-05 13:46:29 +0300

    Update tokenize_jsonl.py

--- tokenize_jsonl.py
+++ tokenize_jsonl.py
@@ -15,17 +15,28 @@ from argparse import ArgumentParser
 
 
 DEFAULT_TOKENIZER = 'TurkuNLP/bert-base-finnish-cased-v1'
+DEFAULT_NORMALIZATION = 'NFKC'
 
 # Escape characters for --include-space
 SPACE_ESCAPE = '<S>'
 TAB_ESCAPE = '<T>'
 NEWLINE_ESCAPE = '<N>'
 
+# Special cases for normalization
+COMBINING_DIACRITICAL_MARKS = ''.join(chr(c) for c in range(0x0300, 0x036F+1))
+REMOVE_COMBINING_CHARS = str.maketrans('', '', COMBINING_DIACRITICAL_MARKS)
+
 
 def argparser():
     ap = ArgumentParser()
+    ap.add_argument('-b', '--bert-vocab', default=None,
+                    help='use given BERT vocab.txt instead of --tokenizer')
+    ap.add_argument('-n', '--normalization', default=DEFAULT_NORMALIZATION,
+                    help='unicode normalization')
     ap.add_argument('-s', '--no-space', default=False, action='store_true',
                     help='do not encode space characters')
+    ap.add_argument('-u', '--allow-unk', default=False, action='store_true',
+                    help='allow UNK token in output')
     ap.add_argument('-t', '--tokenizer', default=DEFAULT_TOKENIZER)
     ap.add_argument('jsonl', nargs='+')
     return ap
@@ -44,6 +55,34 @@ def tokenize(text, tokenizer, args):
         yield tokenizer.tokenize(text)
 
 
+def warn_on_unk(text, tokenizer, tokens):
+    if tokenizer.unk_token in tokens:
+        for token in tokenizer.basic_tokenizer.tokenize(
+                text, never_split=tokenizer.all_special_tokens):
+            if tokenizer.unk_token in tokenizer.tokenize(token):
+                logging.warning(
+                    f'tokenize("{token}") = {tokenizer.tokenize(token)}'
+                    f' ({token.encode("unicode-escape").decode("ascii")})'
+                )
+
+
+def remove_combining_chars(string):
+    return string.translate(REMOVE_COMBINING_CHARS)
+
+
+def normalize(text, args):
+    # Tokenization edge cases
+    text = text.replace(' ́', "'")
+
+    text = unicodedata.normalize(args.normalization, text)
+
+    if args.normalization.endswith('C'):
+        # composition normalization, no combining chars should remain
+        text = remove_combining_chars(text)
+
+    return text
+
+
 def tokenize_jsonl(fn, tokenizer, args):
     with open(fn) as f:
         for ln, line in enumerate(f, start=1):
@@ -53,23 +92,29 @@ def tokenize_jsonl(fn, tokenizer, args):
             except:
                 logging.error(f'parsing line {ln} in {fn}: {line}')
                 raise
+            try:
+                text = normalize(text, args)
+            except:
+                logging.error(f'normalizing line {ln} in {fn}: {text}')
+                raise
             for line_tokens in tokenize(text, tokenizer, args):
+                warn_on_unk(text, tokenizer, line_tokens)
                 print(' '.join(line_tokens))
 
 
 def get_tokenizer(args):
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        args.tokenizer,
-        use_fast=False
-    )
+    if args.bert_vocab is None:
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+            args.tokenizer,
+            use_fast=False
+        )
+    else:
+        tokenizer = transformers.BertTokenizer(
+            args.bert_vocab,
+            do_lower_case=False
+        )
 
-    # Make sure all non-control, non-space 7-bit ASCII chars are included
-    include_chars = [
-        chr(i) for i in range(128)
-        if (not unicodedata.category(chr(i)).startswith('C') and
-            not chr(i).isspace())
-    ]
-    added_count = tokenizer.add_tokens(include_chars)
+    added_count = 0
     if not args.no_space:
         added_count += tokenizer.add_tokens([
             SPACE_ESCAPE, TAB_ESCAPE, NEWLINE_ESCAPE
