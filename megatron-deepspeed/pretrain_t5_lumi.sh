1f6e8ad3fdee1a3a1ef9b8eef391601507cdea59
luukkonenr <36662351+luukkonenr@users.noreply.github.com>
2022-11-18 14:08:19 +0200

    Update pretrain_t5_lumi.sh

--- megatron-deepspeed/pretrain_t5_lumi.sh
+++ megatron-deepspeed/pretrain_t5_lumi.sh
@@ -70,7 +70,7 @@ T5_LARGE_NL36="--num-layers 36 --hidden-size 1024 --num-attention-heads 16 --kv-
 
 ARCHITECTURE=$T5_BASE
 
-CHECKPOINT_PATH=./checkpoints/t5-$ARCHITECTURE-$SLURM_JOBID
+CHECKPOINT_PATH=./checkpoints/t5-$SLURM_JOBID
 mkdir -p $CHECKPOINT_PATH
 
       #  --load $CHECKPOINT_PATH \
